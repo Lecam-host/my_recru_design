b45f108e8804fcd94c91d507c5811bc93f29823e
Lecam-host <lecampapa11@gmail.com>
2025-06-30 10:22:29 +0000

    update

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -19,7 +19,7 @@ import 'package:my_recru_design/presentation/widgets/bottom_navigation_bar.dart'
 class AppRouter extends GoRouter {
   AppRouter()
     : super.routingConfig(
-        initialLocation: PageName.home,
+        initialLocation: PageName.splash,
         observers: <NavigatorObserver>[],
         routingConfig: ValueNotifier<RoutingConfig>(
           RoutingConfig(
